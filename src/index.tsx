c1510b1dcee3fd1a00669fbd3117c9aa230280b4
tetther1122 <tetthera@gmail.com>
2021-02-17 08:56:22 +1000

    export grid theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,5 @@ export { JobBanner, BannerImg, BannerText, SubBanner } from './Banner';
 export { JobInfo } from './Job';
 export { Button } from './Components'
 export { ThemeProvider } from 'styled-components';
+export { GridThemeProvider } from 'styled-bootstrap-grid';
+
